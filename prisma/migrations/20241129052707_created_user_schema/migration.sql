58aa4bed2e1e048c8412ac784ce15281138762b6
unknown <rahulpawar2001.rp@gmail.com>
2024-11-29 13:25:37 +0530

    user schema add

---
+++ prisma/migrations/20241129052707_created_user_schema/migration.sql
@@ -0,0 +1,21 @@
+-- CreateEnum
+CREATE TYPE "AuthType" AS ENUM ('Google', 'Github');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "auth_type" "AuthType" NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
