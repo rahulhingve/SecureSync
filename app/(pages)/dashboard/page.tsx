47ae86a56b6e275e008a8a3470d230d46e6a795f
unknown <rahulpawar2001.rp@gmail.com>
2024-12-08 22:01:54 +0530

    session add

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -1,6 +1,16 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 import Dashboard from "@/components/Dashboard";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function  Home  ()  {
+
+const session = await getServerSession(authOptions)
+
+if(!session){
+  redirect("/login")
+}
+
   return (
     <>
     
--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -1,7 +1,22 @@
+import { authOptions } from '@/app/api/auth/[...nextauth]/options';
 import LoginPage from '@/components/Login'
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const page = () => {
+
+
+
+const page = async  () => {
+
+const session = await getServerSession(authOptions);
+
+if(session){
+console.log("logged in redirecting to dashboard ")
+  redirect("/dashboard")
+
+}
+
   return (
     <LoginPage/>
   )
